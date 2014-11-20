922b85169add53d2ed3e7d7d0ca3338a9754dcab
Vedran Mandić <mandic.vedran@gmail.com>
2014-11-20 20:32:52 +0100

    Updated web api to work properly, WP8.1 is not supported for PushSharp, changing to Windows Store Apps.

--- PushSharpDemo/PushSharp.WebAPI/Filters/CustomAuthorizationFilter.cs
+++ PushSharpDemo/PushSharp.WebAPI/Filters/CustomAuthorizationFilter.cs
@@ -10,8 +10,6 @@ namespace PushSharp.WebAPI.Filters
 {
     public class CustomAuthorizationFilter : System.Web.Http.AuthorizeAttribute
     {
-        private readonly Encoding encoding = Encoding.GetEncoding("iso-8859-1");
-
         public override void OnAuthorization(HttpActionContext actionContext)
         {
             var clientAddress = actionContext.Request.GetClientIpAddress();
@@ -58,7 +56,7 @@ namespace PushSharp.WebAPI.Filters
                     return false;
 
                 string authEncoded = actionContext.Request.Headers.Authorization.Parameter ?? "";
-                string[] credentials = !authEncoded.Equals("") ? DecodeBase64NTimes(authEncoded, 1).Split(':') : new string[] { " ", " " };
+                string[] credentials = !authEncoded.Equals("") ? Helpers.DecodeBase64NTimes(authEncoded, 1).Split(':') : new string[] { " ", " " };
 
                 //validate the apikey from the basic auth key pair
                 return credentials[0] == "devug" && credentials[1] == "pushsharp";
@@ -68,23 +66,6 @@ namespace PushSharp.WebAPI.Filters
                 SimpleErrorLogger.Log(ex);
                 return false;
             }
-        }
-
-        /// <summary>
-        /// Runs Base64 decoding N times
-        /// </summary>
-        /// <param name="value">The base64 string to decode.</param>
-        /// <param name="times">Determines how many times should the entered value be decoded in repetition.</param>
-        /// <returns>A decoded raw string value in format "uname:pwd".</returns>
-        private string DecodeBase64NTimes(string value, int times)
-        {
-            times--;
-            string decoded = encoding.GetString(Convert.FromBase64String(value));
-
-            if (times == 0)
-                return decoded;
-            else
-                return DecodeBase64NTimes(decoded, times);
         }
     }
 }
